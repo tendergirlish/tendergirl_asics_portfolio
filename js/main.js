52046b6d68463d1c7eae07b654b2b710eca7ea15
tendergirlish <88224436+tendergirlish@users.noreply.github.com>
2021-08-04 17:56:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
     });
 
 
-    //드롭다운 메뉴 부분 꽉차게 배경들어가는 부분 + css
+//드롭다운 메뉴 부분 꽉차게 배경들어가는 부분 + css
     $("#header_logo").before("<p class='n_bg'></p>");
     
     
-    //추가태그 숨기기
+//추가태그 숨기기
     $(".n_bg").hide();
 	
-    	//하위ul 메뉴 보이기 /숨기기
+//하위ul 메뉴 보이기 /숨기기
     $(".submenu").hide();
     $(".drop_top > li").hover(function(){
         $(this).children("ul").show();
@@ -28,4 +28,20 @@ $(document).ready(function(){
         $(".n_bg").hide();
      return false;
     });
+	
+	
+//신발 애니메이션 
+	
+	var product = setInterval(function(){
+		$(".s_img1").animate({"bottom":"200px"},1200);
+		$(".s_img1").animate({"bottom":"170px"},1200);
+		
+		$(".s_img2").animate({"bottom":"280px"},1500);
+		$(".s_img2").animate({"bottom":"310px"},1500);
+		
+		$(".s_img3").animate({"top":"890px"},1900);
+		$(".s_img3").animate({"top":"875px"},1900);
+	});
+	
+		
 });//end
