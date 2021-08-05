e3c10e202d08d264ea74a47220b0e260b64ab1bf
tendergirlish <88224436+tendergirlish@users.noreply.github.com>
2021-08-05 17:21:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -44,4 +44,52 @@ $(document).ready(function(){
 	});
 	
 		
+    
+    //bottom border-bottom 
+        $("#key_rolling").after("<p class='border'></p>");
+        $("#bottom_body").after("<p class='border'></p>");
+        $("#copyright").after("<p class='border'></p>");
+    
+    
+//    bottom 키워드 롤링
+        var keyrolling1 = setInterval(function(){            
+            $(".news_wrap ul li:first").slideUp(function(){            
+                $(this).appendTo($(".news_wrap ul")).slideDown();            
+            });                           
+        },3000);
+    
+        var keyrolling2 = setInterval(function(){
+            $(".event_wrap ul li:first").slideUp(function(){            
+                $(this).appendTo($(".event_wrap ul")).slideDown();
+            });
+        },4000);
+    
+      var keyrolling3 = setInterval(function(){        
+        $(".hot_wrap ul li:first").slideUp(function(){            
+            $(this).appendTo($(".hot_wrap ul")).slideDown();
+        });
+      },5000);
+    
+    
+//    top_bottom    
+    var  scrollbtn = setInterval(function(){
+        $("#top_bottom").animate({"top":"51%"},1300);
+        $("#top_bottom").animate({"top":"50%"},1300);
+    });
+    
+    $(".top_ico").click(function(){       
+        $("html, body").stop().animate({scrollTop:0},1000);
+    });        
+    $(".bottom_ico").click(function(){       
+        $("html, body").stop().animate({scrollTop:8000},1000);
+    });
+    
+    
+//    tablet js 추가 
+        $(".b_body_wrap > h2").after("<p class='border_t'></p>");
+        $(".copyright").after("<p class='border_t2'></p>");
+    
+    
+    
+    
 });//end
